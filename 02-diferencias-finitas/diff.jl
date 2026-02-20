8c93f367e7e492822a28a466d83c978012bd0cd7
LeninPA <lenin.pavon@comunidad.unam.mx>
2026-02-19 20:59:29 -0600

    FEAT: Diferencias finitas en julia

---
+++ 02-diferencias-finitas/diff.jl
@@ -0,0 +1,25 @@
+import Statistics: mean
+
+function main()
+  f(x)  = exp(x)
+  df(x) = exp(x)
+
+  x = 0:0.001:10
+  fx = f.(x)
+  h = step(x)
+
+  df_approx = diff(fx) ./ h
+  df_truth_full = df.(x)
+  df_truth = df.(x)[2:length(df_truth_full)]
+  err = abs.(df_approx - df_truth)
+  # println("df(x)")
+  # println(df_truth)
+  # println("f(x)")
+  # println(df_approx)
+  # println("err")
+  # println(err)
+  println("Error promedio")
+  println(mean(err))
+end
+
+main()
